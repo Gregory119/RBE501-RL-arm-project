f4b1806f0e5c4bdfdbb89555753027da2ba5ceda
Gregory Jones <greggie119gj@gmail.com>
2025-06-30 13:19:16 -0400

    Use exponential for reward and never terminate

--- gymnasium_env/arm.py
+++ gymnasium_env/arm.py
@@ -58,9 +58,7 @@ class ArmEnv(MujocoEnv):
             
         observation_space = Box(low=-np.inf, high=np.inf, shape=(15,), dtype=np.float64)
 
-        # self.goal = np.zeros(3, dtype=np.float32) #initialize goal point
         self.goal = np.array([0.5,0.1,0.1])
-        self.goal_radius = 0.02 #how close the ee needs to be to the goal to be considered successful
 
         self.load_data = self.LoadData(xml_file=xml_file,
                                        frame_skip=frame_skip,
@@ -80,7 +78,6 @@ class ArmEnv(MujocoEnv):
             ],
             "render_fps": int(np.round(1.0 / self.dt)),
         }
-        self.reached_goal = False
 
 
     def _load_env(self):
@@ -103,43 +100,26 @@ class ArmEnv(MujocoEnv):
         obs = self._get_obs()
 
         ####### Defining reward
-        # The problem with using the negative distance as the reward is that the
-        # maximum accumulated reward depends on the start configuration. More
-        # specifically, the policy will only achieve a zero accumulated reward
-        # if the robot can move to the goal in one environment step.
-
-        # Instead the maximum accumulated reward can be set by making each
-        # reward equal to the difference in distance from the goal, normalized
-        # by the goal distance at the start of the episode. This ratio will be 1
-        # when the goal is reached (assuming zero tolerance), so it can be
-        # multiplied to give the desired maximum accumulated reward. The only
-        # problem is that the accumulated reward can be slightly larger than the
-        # desired max due to accumulated floating point resolution error in the
-        # dist difference.
+        # Using a decaying exponential function based on the distance from the
+        # goal for the reward gives a maximum reward when the distance to the
+        # goal is zero. The policy is encouraged to move to this state as soon
+        # as possible so that the maximum reward is obtained for each step of
+        # the environment.
         ee_pos = self.data.site("gripper").xpos
         dist = np.linalg.norm(ee_pos - self.goal)
         assert(dist > 0)
         diff_dist = self.prev_dist - dist
-        # if distance difference > 0 => moving toward the goal => reward > 0
-        # if distance difference < 0 => moving away from the goal => reward < 0
-        # if moving away from goal, then double the reward for a higher penalty
+        # # if distance difference > 0 => moving toward the goal => reward > 0
+        # # if distance difference < 0 => moving away from the goal => reward < 0
+        # # if moving away from goal, then penalize the reward more
         scale = 1 if diff_dist > 0 else 2
-        reward = 100*scale*diff_dist/self.start_dist
-        self.prev_dist = dist
 
-        # lets never terminate, only truncate so that when the robot gets to the
-        # goal region it learns to stay there
-        if dist < self.goal_radius and not self.reached_goal:
-            reward += 50.0
-            self.reached_goal = True
-
-        # if the goal is not reached anymore, then remove the goal reward
-        if self.reached_goal and dist > self.goal_radius:
-            reward -= 50.0
-            self.reached_goal = False
+        reward = np.exp(-scale*10*dist)
 
         truncated = self.steps >= self.max_episode_steps
-        terminated = truncated and self.reached_goal
+        # never terminate so that the policy keeps trying to improve even when
+        # the goal region is reached
+        terminated = False
 
         info = {
             "terminated": terminated,
@@ -174,7 +154,6 @@ class ArmEnv(MujocoEnv):
     # override
     def reset_model(self):
         self.steps=0
-        self.reached_goal=False
         #Randomization of goal point
         self.goal = self._sample_goal()
         self._load_env()
