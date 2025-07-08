dc8849bda9063e3f3ad69db20cf286ac025f9ba2
Gregory Jones <greggie119gj@gmail.com>
2025-07-08 12:52:18 -0400

    harware communication working with env

--- gymnasium_env/arm_hw.py
+++ gymnasium_env/arm_hw.py
@@ -4,30 +4,37 @@ import mujoco
 import gymnasium as gym
 import copy
 import time
+import logging
+from pprint import pformat
 
 from gymnasium.envs.mujoco import MujocoEnv
 from gymnasium.spaces import Box
 from dataclasses import dataclass
 
+from lerobot.common.utils.utils import init_logging
+
 from lerobot.common.robots import (
-    Robot,
     RobotConfig,
     make_robot_from_config,
     so101_follower,
 )
+from lerobot.common.robots.so101_follower import SO101FollowerConfig, SO101Follower
+
+from .arm import Arm
 
 
-class ArmHw(gym.Env):
+class ArmHwEnv(gym.Env):
 
     def __init__(self,
-                 robot_cfg,
-                 xml_file: str | None = None,
                  rate_hz: int = 250,
                  enable_normalize = True,
                  enable_terminate = False,
                  ):
-        # overwrite robot configuration to use degrees (radians is not an option)
-        robot_cfg.use_degrees = True
+        robot_cfg = SO101FollowerConfig(id="follower_arm",
+                                        port="/dev/ttyACM0",
+                                        # (radians is not an option)
+                                        use_degrees=True)
+        self.robot = SO101Follower(robot_cfg)
 
         self.robot = make_robot_from_config(robot_cfg)
         self.robot.connect()
@@ -35,10 +42,10 @@ class ArmHw(gym.Env):
         # load the robot model
         xml_file = path.join(
             path.dirname(__file__),
-            "gymnasium_env/SO-ARM100/Simulation/SO101/scene.xml"
+            "SO-ARM100/Simulation/SO101/scene.xml"
             )
         self.model = mujoco.MjModel.from_xml_path(xml_file)
-        self.data = mujoco.MjData(model)
+        self.data = mujoco.MjData(self.model)
 
         get_pos_fn = self.read_pos
         get_vel_fn = self.read_vel
@@ -46,10 +53,9 @@ class ArmHw(gym.Env):
         should_truncate_fn = lambda: not self.in_bounds()
         visualize = lambda: None
 
-        observation_space = None
+        self.observation_space = None
         def set_obs_space(obs_space):
-            nonlocal observation_space
-            observation_space = obs_space
+            self.observation_space = obs_space
 
         self.arm = Arm(rate_hz=rate_hz,
                        get_pos_fn=get_pos_fn,
@@ -62,6 +68,11 @@ class ArmHw(gym.Env):
                        enable_normalize=enable_normalize,
                        enable_terminate=enable_terminate)
 
+        # Set the action space (copied from mujoco environment).
+        bounds = self.model.actuator_ctrlrange.copy().astype(np.float32)
+        low, high = bounds.T
+        self.action_space = Box(low=low, high=high, dtype=np.float32)
+
 
     def read_pos(self):
         q_dict = self.robot.bus.sync_read("Present_Position")
@@ -72,7 +83,7 @@ class ArmHw(gym.Env):
 
     def read_vel(self):
         dq_dict = self.robot.bus.sync_read("Present_Velocity")
-        dq_deg_p_s = np.array([v for _,v in q_dict.items()])
+        dq_deg_p_s = np.array([v for _,v in dq_dict.items()])
         dq = dq_deg_p_s / 180 * np.pi
         return dq
 
@@ -81,22 +92,32 @@ class ArmHw(gym.Env):
         q = self.read_pos()
         self.data.qpos[:] = q
         mujoco.mj_forward(self.model,self.data)
-        ee_pos = data.site("gripper").xpos
+        ee_pos = self.data.site("gripper").xpos
         return self.arm.in_bounds(ee_pos)
 
 
-    def _send_action(self, action):
+    def send_action(self, action):
+        np_action = np.array(action) / np.pi * 180
+        if not self.robot.is_connected:
+            return
         keys = ['shoulder_pan.pos', 'shoulder_lift.pos', 'elbow_flex.pos', 'wrist_flex.pos', 'wrist_roll.pos', 'gripper.pos']
-        self.robot.send_action(dict(zip(keys, action)))
+        # the unit of each action element is radian so convert to degree
+        self.robot.send_action(dict(zip(keys, np_action)))
 
 
-    def reset():
-        self.load_env()
+    def reset(self,
+              seed: int | None = None,
+              options: dict | None = None,
+              ):
+        super().reset(seed=seed)
+        obs = self.arm.reset(mj_model=self.model, mj_data=self.data)
+        info = {}
+        return obs, info
 
 
     def load_env(self):
         # robot should already be connected so just reset the position
-        self._send_action([0,0,0,0,0,0])
+        self.send_action([0,0,0,0,0,0])
 
         # By default the robot doesn't use integral gain so the joint position
         # error can be large and depend on the robot configuration. Using joint
@@ -121,10 +142,14 @@ class ArmHw(gym.Env):
 
 
     def step(self, action):
+        # This needs to replicate how the simulation environment works, which
+        # first steps the simulation and then sends the action. On hardware this
+        # is the same as first sleeping and then sending the action.
         self.arm.step_sleep(display_rate=True)
-        self.robot.send_action(action)
+        self.send_action(action)
         self.arm.step(action, mj_model=self.model, mj_data=self.data)
 
 
     def __del__(self):
-        self.robot.disconnect()
+        if hasattr(self,"robot"):
+            self.robot.disconnect()
